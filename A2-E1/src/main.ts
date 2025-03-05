7c4174ada47c188f641be0608a2bb015ff2e7178
Theo <69534996+TheoGomide@users.noreply.github.com>
2025-03-05 20:00:16 -0300

    Update main.ts

--- A2-E1/src/main.ts
+++ A2-E1/src/main.ts
@@ -1,2 +1,26 @@
-const sg = "Hello, World!";
-console.log(msg);
+class Retangulo {
+    private largura: number;
+    private altura: number;
+
+    constructor(largura: number, altura: number) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    private calculoArea(): number {
+        return this.largura * this.altura;
+    }
+
+    public retorno(): void {
+        const area = this.calculoArea();
+        console.log(`A área: ${area}.`);
+    }
+}
+
+function main(): void {
+    const retangulo = new Retangulo(10, 10);
+
+    retangulo.retorno();
+}
+
+main();
